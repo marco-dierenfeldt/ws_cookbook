ae222b61b9161e1da159bf2a5f02a34ee45bf4be
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2021-10-21 16:19:49 +0200

    first component hardcoded

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Recepie from './components/recepie.component';
 function App() {
   return (
     <div>
-      <div className="siimple-navbar">
+      <div className="siimple-navbar siimple-navbar-dark">
         <div className="siimple-navbar-title">Mein Kochbuch</div>
       </div>
-      <div className="siimple-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
-        <Recepie/>
+      <div className="siimple-content siimple--display-flex siimple--px-4">
+        <Recepie />
       </div>
-      <div className="siimple-footer siimple-footer--dark">
+      <div className="siimple-footer siimple-footer--small siimple-footer--dark siimple--text-center">
         Made with love by <strong>Marco Dierenfeldt</strong>
       </div>
     </div>
