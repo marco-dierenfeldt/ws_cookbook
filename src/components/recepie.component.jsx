987fb0aaa4c74c264418fd2133422cedbdf8784d
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2021-10-21 16:36:12 +0200

    introduce and use state object

--- src/components/recepie.component.jsx
+++ src/components/recepie.component.jsx
@@ -2,57 +2,36 @@ import { Component } from "react";
 import './recepie.component.css';
 
 class Recepie extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            recepie:{
+                name:"Rezeptüberschrift oder Rezeptname",
+                ingredients:['100g Zucker','5g Salz','400g Mehl','2  Eier'],
+                process:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.'
+            }
+        }
+    }
     render = () => {
         return (
             <div className="recepie">
-                <h2>Rezeptüberschrift oder Rezeptname</h2>
+                <h2>{this.state.recepie.name}</h2>
                 <div className="ingredients">
 
                     <div className="siimple-list">
                         <div className="siimple-list-item">
                             <div className="siimple-list-title">Zutaten</div>
                         </div>
-                        <div className="siimple-list-item">100g Zucker</div>
-                        <div className="siimple-list-item">5g Salz</div>
-                        <div className="siimple-list-item">400g Mehl</div>
-                        <div className="siimple-list-item">2  Eier</div>
-
+                        {this.state.recepie.ingredients.map((ingredient) => {
+                            return <div className="siimple-list-item">{ingredient}</div>;
+                        })}
                     </div>
                 </div>
                 <div className="process">
                     <h3>Zubereitung</h3>
                     <div>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                            natoque penatibus et magnis dis parturient montes, nascetur
-                            ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                            eu, pretium quis, sem. Nulla consequat massa quis enim.
-                            Donec pede justo, fringilla vel, aliquet nec, vulputate
-                            eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis
-                            vitae, justo. Nullam dictum felis eu pede mollis pretium.
-                        </p>
-                        <p>
-                            Integer tincidunt. Cras dapibus. Vivamus elementum semper
-                            nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-                            porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
-                            lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-                            Phasellus viverra nulla ut metus varius laoreet. Quisque
-                            rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
-                            Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam
-                            rhoncus.
-                        </p>
-                        <p>
-                            Maecenas tempus, tellus eget condimentum rhoncus, sem quam
-                            semper libero, sit amet adipiscing sem neque sed ipsum. Nam
-                            quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-                            Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien
-                            ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet
-                            orci eget eros faucibus tincidunt. Duis leo. Sed fringilla
-                            mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat,
-                            leo eget bibendum sodales, augue velit cursus nunc.
-                        </p>
+                        {this.state.recepie.process}
                     </div>
                 </div>
             </div>
