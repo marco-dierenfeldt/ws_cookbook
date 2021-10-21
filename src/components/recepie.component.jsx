244edd995e9f886c1626d38bedb57275498de95f
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2021-10-21 15:07:35 +0200

    add first service and component

---
+++ src/components/recepie.component.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class RecepieListView extends Component {
+    render = () => {
+        return(
+            <div>RecepieListView Component</div>
+        )
+    }
+}
+
+export default RecepieListView;
