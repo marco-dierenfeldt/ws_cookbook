fe6fa42af27b017cb4b97d7a03cb9756b455b4a6
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2021-10-21 16:19:49 +0200

    first component hardcoded

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Recipe from './components/recipe.component'
 function App() {
   return (
     <div>
-      <div className="siimple-navbar">
+      <div className="siimple-navbar siimple-navbar-dark">
         <div className="siimple-navbar-title">Mein Kochbuch</div>
       </div>
-      <div className="siimple-content">
-        <Recipe/>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+      <div className="siimple-content siimple--display-flex siimple--px-4">
+        <Recipe />
       </div>
-      <div className="siimple-footer siimple-footer--dark">
+      <div className="siimple-footer siimple-footer--small siimple-footer--dark siimple--text-center">
         Made with love by <strong>Marco Dierenfeldt</strong>
       </div>
     </div>
