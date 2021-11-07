e8edf5ff7ac745705ac8308304210a3e9971c59d
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2021-11-07 22:11:41 +0100

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import RecepieList from './components/recepie_list.component';
+import RecipeList from './components/recipe_list.component';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <div className="siimple-navbar-title">Mein Kochbuch</div>
       </div>
       <div className="siimple-content siimple--display-flex siimple--px-4">
-        <RecepieList/>        
+        <RecipeList/>        
       </div>
       <div className="siimple-footer siimple-footer--small siimple-footer--dark siimple--text-center">
         Made with love by <strong>Marco Dierenfeldt</strong>
