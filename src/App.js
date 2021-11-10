c1fdb9e17e53682eebee1e25848602c257161d6e
marco-dierenfeldt <marco.dierenfeldt@ausy-technologies.de>
2021-11-10 21:15:18 +0100

    fix footer position remove component from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import Recipe from './components/recipe.component'
 
 function App() {
   return (
-    <div>
+    <div id="page-container">
       <div className="siimple-navbar">
         <div className="siimple-navbar-title">Mein Kochbuch</div>
       </div>
-      <div className="siimple-content">
-        <Recipe/>
+      <div className="siimple-content" id="content-wrap">
+        Hier kommt der Inhalt...
         Lorem ipsum dolor sit amet, consectetur adipiscing elit...
       </div>
-      <div className="siimple-footer siimple-footer--dark">
+      <div className="siimple-footer siimple-footer--dark" id="footer">
         Made with love by <strong>Marco Dierenfeldt</strong>
       </div>
     </div>
