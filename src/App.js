2a80ee2494896917b64323a345215e351f4f499a
Marco Dierenfeldt <marco.dierenfeldt@ausy-technologies.de>
2022-12-29 23:10:28 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div id="page-container">
-      <div className="siimple-navbar">
-        <div className="siimple-navbar-title">Mein Kochbuch</div>
-      </div>
-      <div className="siimple-content" id="content-wrap">
-        Hier kommt der Inhalt...
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
-      </div>
-      <div className="siimple-footer siimple-footer--dark" id="footer">
-        Made with love by <strong>Marco Dierenfeldt</strong>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
