204e578cf671aec9f3feba9b4c23a2b9e89f1292
Marco Dierenfeldt <marco.dierenfeldt@ausy-technologies.de>
2022-12-30 15:37:01 +0100

    fix className issue

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 function App() {
   return (
     <div>
-      <section class="hero is-primary">
-        <div class="hero-body">
-          <p class="title">
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <p className='title'>
             Mein Kochbuch
           </p>
-          <p class="subtitle">
+          <p className='subtitle'>
             Workshop am 13.01.2023
           </p>
         </div>
       </section>
-      <section id='main_content' className='section'>
+      <section id="main_content" className='section'>
         <p>Lorem ipsum dolor sit amet...</p>
       </section>
-      <footer class="footer is-align-self-baseline" id='footer'>
-        <div class="content has-text-centered">
+      <footer className='footer is-align-self-baseline' id="footer">
+        <div className='content has-text-centered'>
           <p>
             made with &#10084; by <a href="mailto:marco.dierenfeldt@ausy-technologies.de">Marco Dierenfeldt</a>.
           </p>
