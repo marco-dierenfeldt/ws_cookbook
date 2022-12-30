bff09a49f11514dbd1dbbb2dfa6304991e68c639
Marco Dierenfeldt <marco.dierenfeldt@ausy-technologies.de>
2022-12-30 14:54:17 +0100

    put datahandling into service

--- src/components/recipe.component.jsx
+++ src/components/recipe.component.jsx
@@ -1,17 +1,24 @@
 import { Component } from "react";
+import RecipeService from "../service/recepie-service";
 
 class Recipe extends Component {
     constructor() {
         super();
 
         this.state = {
-            recipe:{
-                name:"Rezeptüberschrift oder Rezeptname",
-                ingredients:['100g Zucker','5g Salz','400g Mehl','2  Eier'],
-                process:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.'
+            recipe: {
+                name: '',
+                ingredients: [],
+                process: ''
             }
         }
     }
+
+    componentDidMount = () => {
+        var recipe = RecipeService.getRecipe();
+        this.setState({recipe});
+    }
+
     render = () => {
         return (
             <div class="card">
--- src/service/recepie-service.js
+++ src/service/recepie-service.js
@@ -1,9 +1,19 @@
-class RecepieService {
-    recepieList = [];
+class RecipeService {
+    recipeList = [
+        {
+            name:"Rezeptüberschrift oder Rezeptname",
+            ingredients:['100g Zucker','5g Salz','400g Mehl','2  Eier'],
+            process:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.'
+        }
+    ];
 
-    getAllRecepies(){
-        return this.recepieList;
+    getAllRecipes() {
+        return this.recipeList;
+    }
+
+    getRecipe() {
+        return this.recipeList[0];
     }
 }
 
-export default new RecepieService();
+export default new RecipeService();
